982dde8a316b2498348ea931a8ad72a475d88609
Emanuel Costa <emanuelldsc@gmail.com>
2023-04-11 21:01:31 -0300

    Timer alarm

--- lib/widgets/my_timer.dart
+++ lib/widgets/my_timer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -7,32 +8,31 @@ class MyTimer extends StatelessWidget {
   const MyTimer({
     Key? key,
     required CountDownController controller,
-  }) : _controller = controller, super(key: key);
+  })  : _controller = controller,
+        super(key: key);
 
   final CountDownController _controller;
 
   @override
   Widget build(BuildContext context) {
     return CircularCountDownTimer(
-        autoStart: false,
-        isReverse: true,
-        controller: _controller,
-        width: 100,
-        height: 100,
-        duration: 30,
-        fillGradient: LinearGradient(colors: 
-        [Colors.red.shade700, Colors.white, Colors.white]),
-        fillColor: Colors.black, 
-        ringColor: kButtonColor,
-        strokeWidth: 2.5,
-        textStyle: 	GoogleFonts.lato(
-        fontSize: 24,
-        color: kLetterColor,
-        fontWeight: FontWeight.w700),
-        textFormat: CountdownTextFormat.MM_SS,
-        onComplete: () {
-          
-        },
-        );
+      autoStart: false,
+      isReverse: true,
+      controller: _controller,
+      width: 100,
+      height: 100,
+      duration: 30,
+      fillGradient: LinearGradient(
+          colors: [Colors.red.shade700, Colors.white, Colors.white]),
+      fillColor: Colors.black,
+      ringColor: kButtonColor,
+      strokeWidth: 2.5,
+      textStyle: GoogleFonts.lato(
+          fontSize: 24, color: kLetterColor, fontWeight: FontWeight.w700),
+      textFormat: CountdownTextFormat.MM_SS,
+      onComplete: () {
+        FlutterRingtonePlayer.play(fromAsset: "assets/alarm.wav"); 
+      },
+    );
   }
 }
