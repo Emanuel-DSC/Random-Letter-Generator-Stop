737f22bc69cf94dc83a1cc2ebd7a5c157d3008ed
Emanuel Costa <emanuelldsc@gmail.com>
2023-05-01 17:47:41 -0300

    Final changes

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -33,56 +33,59 @@ class SettingsState extends State<Settings> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBgColor,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          LatoText(
-              size: 26,
-              text: AppLocalizations.of(context)!.kSettingsText.toUpperCase()),
-          const SizedBox(height: 22),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              LatoText(
-                  size: 18,
-                  text: AppLocalizations.of(context)!.swipe.toUpperCase()),
-              const SizedBox(width: 15),
-              Icon(
-                (FontAwesomeIcons.arrowDownLong),
-                color: kLetterColor,
-              ),
-            ],
-          ),
-          const SizedBox(height: 30),
-          const SizedBox(
-            height: 200,
-            child: TimerPicker(),
-          ),
-          const SizedBox(height: 50),
-          LatoText(
-              size: 22,
-              text: AppLocalizations.of(context)!.kSettingsText3.toUpperCase()),
-          const SizedBox(height: 5),
-          Switch(
-            activeColor: kResetColor,
-            inactiveTrackColor: kButtonColor,
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-                switchData.write('isSwitched', isSwitched);
-                if (isSwitched == false) {
-                  MyTimer.vol = 1;
-                } else {
-                  MyTimer.vol = 0;
-                }
-              });
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            LatoText(
+                size: 26,
+                text: AppLocalizations.of(context)!.kSettingsText.toUpperCase()),
+            const SizedBox(height: 22),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                LatoText(
+                    size: 18,
+                    text: AppLocalizations.of(context)!.swipe.toUpperCase()),
+                const SizedBox(width: 15),
+                Icon(
+                  (FontAwesomeIcons.arrowDownLong),
+                  color: kLetterColor,
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const SizedBox(
+              height: 200,
+              child: TimerPicker(),
+            ),
+            const SizedBox(height: 50),
+            LatoText(
+                size: 22,
+                text: AppLocalizations.of(context)!.kSettingsText3.toUpperCase()),
+            const SizedBox(height: 5),
+            Switch(
+              activeColor: kResetColor,
+              inactiveTrackColor: kButtonColor,
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                  switchData.write('isSwitched', isSwitched);
+                  if (isSwitched == false) {
+                    MyTimer.vol = 1;
+                  } else {
+                    MyTimer.vol = 0;
+                  }
+                });
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+        ),
       ),
     );
   }
